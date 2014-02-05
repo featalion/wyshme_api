d2139ce6fd6f813f582fc5519c8e3310c63ece4e
DanielDimitrov <dimitrov.dan@gmail.com>
2014-02-05 17:33:02 +0000

    Changed email text
    Signed-off-by: DanielDimitrov <dimitrov.dan@gmail.com>

--- app/mailers/user_shares_content_mailer.rb
+++ app/mailers/user_shares_content_mailer.rb
@@ -6,9 +6,10 @@ class UserSharesContentMailer < ActionMailer::Base
     @url = "http://wyshme.herokuapp.com/shares/#{content_share.hash_id}"
     @type_of_content = content_share.content_type
     @user_message = content_share.message
+    @recipients = content_share.recipients
 
     mail to: content_share.recipients,
-         subject: "User shared #{@type_of_content} on WyshMe!"
+         subject: "#{@user.email} has shared #{@type_of_content} with you on WyshMe!"
   end
 
 end
